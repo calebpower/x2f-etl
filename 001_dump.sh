453677c12851a752593f9c5c31ff4b46f7ceb8f8
Caleb L. Power <cpower@axonibyte.com>
2025-01-03 19:17:37 -0500

    check for xenforo install

--- 001_dump.sh
+++ 001_dump.sh
@@ -1,5 +1,10 @@
 #!/usr/local/bin/bash
 
+if [ ! -f xenforo/index.php ]; then
+  printf 'You need to drop the Xenforo site in the "xenforo" folder.\n' >&2
+  exit 1
+fi
+
 for file in $(ls scripts | grep dump_xenforo); do
   python scripts/$file
 done
